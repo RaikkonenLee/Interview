e34c983fab2b3983f16aa31e9d2b56cfb9d7831b
Raikkonen Lee <leewei0528@gmail.com>
2021-07-07 14:50:07 +0800

    新增NorthwindDB Model及Repository

---
+++ YungChing/Models/Respository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace YungChing.Models
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IList<T>> GetAll();
+        Task<IList<T>> Where(T entity, Expression<Func<T, bool>> expression);
+        Task<T> Get(int id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(int id);
+        Task<T> Delete(T entity);
+    }
+}
---
+++ YungChing/Models/Respository/OrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YungChing.Models
+{
+    public class OrderRepository : Repository<Order, NorthwindContext>
+    {
+        public OrderRepository(NorthwindContext context) : base(context)
+        {
+        }
+    }
+}
